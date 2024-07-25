2870e2bbec84cc673cce408cd1cb7f945249e2d0
mbfuss <145613641+mbfuss@users.noreply.github.com>
2024-07-25 16:34:05 +0400

    Initial commit

---
+++ fs.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
+
+type FileInfoWithSize struct {
+	os.FileInfo
+	Size int64
+}
+
+func main() {
+	start := time.Now()
+
+	root, sortOrder := parseFlags()
+
+	// Получения списка файлов из каталога
+	files, err := ioutil.ReadDir(*root)
+	if err != nil {
+		fmt.Printf("Ошибка чтения директория: %v\n", err)
+		return
+	}
+	// Вывод списка файлов из каталога
+	processFiles(*root, files)
+
+	// Обработка файлов и директорий для получения размеров
+	fileInfoWithSizes := processFiles(*root, files)
+
+	// Сортировка файлов и директорий по размеру
+	sortFiles(fileInfoWithSizes, *sortOrder)
+
+	// Вывод результатов в виде таблицы
+	fmt.Printf("%-10s %-30s %15s\n", "Type", "Name", "Size")
+	for _, fileInfo := range fileInfoWithSizes {
+		// Определение типа: файл или директория
+		fileType := "File"
+		if fileInfo.IsDir() {
+			fileType = "Directory"
+		}
+		// Вывод информации о файле или директории
+		fmt.Printf("%-10s %-30s %15s\n", fileType, fileInfo.Name(), formatSize(fileInfo.Size))
+	}
+
+	duration := time.Since(start)
+	fmt.Println("Время выполнения программы", duration)
+}
+
+// parseFlags - функция для создания флагов и проверки их на валидность
+func parseFlags() (root *string, sortOrder *string) {
+	root = flag.String("root", "", "Путь до корневой директории")
+	sortOrder = flag.String("sort", "", "Порядок сортировки: asc (возрастание) или des (убывание)")
+	flag.Parse()
+
+	if *root == "" || *sortOrder == "" {
+		flag.Usage()
+		log.Fatal("Флаги переданы неверно")
+	}
+
+	return root, sortOrder
+}
+
+// processFiles - принимает корневую директорию и список файлов/директорий, вычисляет размер каждого элемента,
+// и возвращает список структур FileInfoWithSize, которые содержат информацию о файлах/директориях и их размерах.
+func processFiles(root string, files []os.FileInfo) []FileInfoWithSize {
+	var result []FileInfoWithSize
+	for _, file := range files {
+		// Полный путь к файлу или директории
+		fullPath := filepath.Join(root, file.Name())
+		size := file.Size()
+		if file.IsDir() {
+			// Если это директория, вычисляем её размер
+			size = getDirSize(fullPath)
+		}
+		// Добавление информации о файле или директории в результат
+		result = append(result, FileInfoWithSize{file, size})
+	}
+	return result
+}
+
+// getDirSize - рекурсивная функция, которая вычисляет размер директории
+func getDirSize(path string) int64 {
+	var size int64
+	// Walk проходит по файловому дереву, укорененному в root, вызывая fn для каждого файла или каталога в дереве, включая root
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Если это файл, добавляем его размер к общему размеру
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Ошибка чтения директория в рекурсивной функции: %v\n", err)
+	}
+	return size
+}
+
+// Функция для сортировки файлов и директорий по размеру
+func sortFiles(files []FileInfoWithSize, order string) {
+	sort.Slice(files, func(i, j int) bool {
+		if order == "asc" {
+			// Сортировка по возрастанию
+			return files[i].Size < files[j].Size
+		}
+		// Сортировка по убыванию
+		return files[i].Size > files[j].Size
+	})
+}
+
+// Функция для форматирования размера файла или директории в читаемый вид
+func formatSize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		// Если размер меньше 1024 байт, выводим в байтах
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	// Цикл для определения подходящей единицы измерения (КБ, МБ, ГБ и т.д.)
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	// Форматированный вывод с одной цифрой после запятой
+	return fmt.Sprintf("%.1f %cB", float64(size)/float64(div), "KMGTPE"[exp])
+}
