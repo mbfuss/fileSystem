4becd2e2d16d8771a9924a9b7f36f5717d2e9233
mbfuss <145613641+mbfuss@users.noreply.github.com>
2024-07-26 13:59:16 +0400

    init

--- fs.go
+++ fs.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"sort"
@@ -13,51 +14,59 @@ import (
 
 // FileInfoWithSize - структура, которая комбинирует информацию о файле с дополнительным полем для хранения размера файла или директории.
 type FileInfoWithSize struct {
-	Name   string // Имя файла
-	IsFile bool   // Тип файла
-	Size   int64  // Размер файла
+	Name   string `json:"name"`    // Имя файла
+	IsFile bool   `json:"is_file"` // Тип файла
+	Size   int64  `json:"size"`    // Размер файла
 }
 
 const desc = "desc"
 const asc = "asc"
 
 func main() {
+	// Регистрация обработчика для пути /fs:
+	http.HandleFunc("/fs", handleFileRequest)
+	fmt.Println("Сервер запущен на порту 80")
+	// Запускает сервера, который будет прослушивать порт 80
+	// nil -- использовать глобальный маршрутизатор http.DefaultServeMux для обработки запросов.
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+// handleFileRequest - функция, которая обрабатывает http запросы
+func handleFileRequest(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	// Создание флагов и получение их значений
-	root, sortOrder := parseFlags()
+
+	// Получение параметров запроса
+	root := r.URL.Query().Get("root")
+	sortOrder := r.URL.Query().Get("sort")
+
+	// Проверка параметров запроса
+	if root == "" || (sortOrder != asc && sortOrder != desc) {
+		http.Error(w, "Неверные параметры запроса", http.StatusBadRequest)
+		return
+	}
 
 	// Получение списка файлов из каталога
-	entries, err := os.ReadDir(*root)
+	entries, err := os.ReadDir(root)
 	if err != nil {
-		fmt.Printf("Ошибка чтения директории: %v\n", err)
+		http.Error(w, fmt.Sprintf("Ошибка чтения директории: %v", err), http.StatusInternalServerError)
 		return
 	}
 
 	// Обработка файлов и директорий для получения размеров
-	fileInfoWithSizes := processFiles(*root, entries)
+	fileInfoWithSizes := processFiles(root, entries)
 
 	// Сортировка файлов и директорий по размеру
-	sortFiles(fileInfoWithSizes, *sortOrder)
+	sortFiles(fileInfoWithSizes, sortOrder)
 
-	// Вывод результатов в виде таблицы
-	printFileInfoTable(fileInfoWithSizes)
-
-	duration := time.Since(start)
-	fmt.Println("Время выполнения программы", duration)
-}
-
-// parseFlags - функция для создания флагов и проверки их на валидность
-func parseFlags() (root *string, sortOrder *string) {
-	root = flag.String("root", "", "Путь до корневой директории")
-	sortOrder = flag.String("sort", "asc", "Порядок сортировки: asc (возрастание) или desc (убывание)")
-	flag.Parse()
-
-	if *root == "" || (*sortOrder != asc && *sortOrder != desc) {
-		flag.Usage()
-		log.Fatal("Флаги переданы неверно")
+	// Форматирование данных в JSON
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(fileInfoWithSizes); err != nil {
+		http.Error(w, fmt.Sprintf("Ошибка кодирования в JSON: %v", err), http.StatusInternalServerError)
+		return
 	}
 
-	return root, sortOrder
+	duration := time.Since(start)
+	fmt.Printf("Обработка запроса заняла: %v\n", duration)
 }
 
 // processFiles - принимает корневую директорию и список файлов/директорий, вычисляет размер каждого элемента,
@@ -69,28 +78,20 @@ func processFiles(root string, entries []os.DirEntry) []FileInfoWithSize {
 		wg.Add(1)
 		go func(i int, entry os.DirEntry) {
 			defer wg.Done()
-			// Полный путь к файлу или директории
 			fullPath := filepath.Join(root, entry.Name())
-			// Получаем информацию о каждом файле
 			fileInfo, err := entry.Info()
 			if err != nil {
 				fmt.Printf("Ошибка получения информации о файле: %v\n", err)
 				return
 			}
-			// Получаем размер текущего элемента
 			size := fileInfo.Size()
-			// Если это не директория, то это файл
 			isFile := !entry.IsDir()
 			if !isFile {
-				// Если это директория, вычисляем её размер
 				size, err = getDirSize(fullPath)
 				if err != nil {
 					fmt.Printf("Ошибка чтения директории в рекурсивной функции: %v\n", err)
 				}
 			}
-			// Добавление информации о файле или директории в результат
-			// Индексы для записей: Каждой горутине передаётся индекс i, который соответствует позиции в срезе result. Это гарантирует,
-			// что каждая горутина будет записывать результат в уникальное место в срезе, тем самым предотвращая конкуренцию.
 			result[i] = FileInfoWithSize{Name: entry.Name(), IsFile: isFile, Size: size}
 		}(i, entry)
 	}
@@ -98,18 +99,18 @@ func processFiles(root string, entries []os.DirEntry) []FileInfoWithSize {
 	return result
 }
 
-// getDirSize - функция которая вычисляет размер директории
+// getDirSize - функция, которая вычисляет размер директории
 func getDirSize(path string) (int64, error) {
 	var size int64
-	// Рекурсивно проходит по всем файлам и поддиректориям, начиная с указанного пути (path)
-	// Анонимная функция вызывается для каждого файла и директории, найденных во время обхода
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		// Если это файл, добавляем его размер к общему размеру
 		if !info.IsDir() {
 			size += info.Size()
+		}
+		if info.IsDir() {
+			size += 4000
 		}
 		return nil
 	})
@@ -117,48 +118,12 @@ func getDirSize(path string) (int64, error) {
 	return size, err
 }
 
-// Функция для сортировки файлов и директорий по размеру
+// sortFiles - функция для сортировки файлов и директорий по размеру
 func sortFiles(files []FileInfoWithSize, order string) {
 	sort.Slice(files, func(i, j int) bool {
 		if order == "asc" {
-			// Сортировка по возрастанию
 			return files[i].Size < files[j].Size
 		}
-		// Сортировка по убыванию
 		return files[i].Size > files[j].Size
 	})
-}
-
-// Функция для форматирования размера файла или директории в читаемый вид
-func formatSize(size int64) string {
-	const unit = 1000
-	if size < unit {
-		// Если размер меньше 1 килобайта, выводим в байтах
-		return fmt.Sprintf("%d B", size)
-	}
-	// div: переменная для хранения текущего масштаба единицы измерения. Изначально устанавливается в 1000 (1 килобайт)
-	// exp: переменная для хранения экспоненты, указывающей на текущую единицу измерения. Изначально установлена в 0 (для байт)
-	div, exp := int64(unit), 0
-	// Цикл с делением размера файла на единицу измерения (1024)
-	for n := size / unit; n >= unit; {
-		n /= unit
-		div *= unit
-		exp++
-	}
-	// Форматированный вывод с одной цифрой после запятой
-	return fmt.Sprintf("%.1f %cB", float64(size)/float64(div), "KMGT"[exp])
-}
-
-// Функция для вывода информации о файлах и директориях в виде таблицы
-func printFileInfoTable(fileInfoWithSizes []FileInfoWithSize) {
-	fmt.Printf("%-10s %-30s %15s\n", "Тип", "Имя", "Размер")
-	for _, fileInfo := range fileInfoWithSizes {
-		// Определение типа: файл или директория
-		fileType := "Файл"
-		if !fileInfo.IsFile {
-			fileType = "Дир"
-		}
-		// Вывод информации о файле или директории
-		fmt.Printf("%-10s %-30s %15s\n", fileType, fileInfo.Name, formatSize(fileInfo.Size))
-	}
 }
