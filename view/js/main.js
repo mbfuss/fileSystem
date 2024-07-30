af23e8a124082390670fcd4e94bda8545834d95b
mbfuss <145613641+mbfuss@users.noreply.github.com>
2024-07-30 15:19:53 +0400

    Кнопка "назад" теперь работает корректно

--- view/js/main.js
+++ view/js/main.js
@@ -1,8 +1,13 @@
+// js/main.js
+
 // Импортируем необходимые модули и функции
 import { fetchData } from './fetchData.js';
 import { updateTable } from './updateTable.js';
 import { navigateToDirectory, navigateBack } from './navigate.js';
 import { addEventHandlers } from './eventHandlers.js';
+
+const asc = "asc";
+const desc = "desc";
 
 // Ждем загрузки DOM перед выполнением скрипта
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,24 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentPath = document.getElementById('currentPath'); // Элемент для отображения текущего пути
     const cancelButton = document.getElementById('cancelButton'); // Кнопка для перехода назад
 
-    // Переменная для хранения текущего пути.
+    // Переменная для хранения текущего пути
     let currentRoot = '/';
 
-    // Функция для обновления текущего пути.
+    // Функция для обновления текущего пути
     const setCurrentRoot = (newRoot) => {
         currentRoot = newRoot;
     };
 
+    // Функция для получения текущего пути
+    const getCurrentRoot = () => currentRoot;
+
     // Функция для получения данных с сервера и обновления таблицы
     const fetchAndUpdateTable = () => {
-        // Получаем порядок сортировки из слайдера.
-        const sortOrder = sortOrderSlider.value === "0" ? "desc" : "asc";
-        // Получаем данные с сервера и обновляем таблицу.
-        fetchData(currentRoot, sortOrder, (data) => updateTable(data, fileTableBody, navigateToDirectory(currentRoot, setCurrentRoot, fetchAndUpdateTable)), currentPath);
+        // Получаем порядок сортировки из слайдера
+        const sortOrder = sortOrderSlider.value === "0" ? desc : asc;
+        // Получаем данные с сервера и обновляем таблицу
+        fetchData(currentRoot, sortOrder, (data) => updateTable(data, fileTableBody, navigateToDirectory(getCurrentRoot, setCurrentRoot, fetchAndUpdateTable)), currentPath);
     };
 
     // Добавляем обработчики событий к элементам управления.
-    addEventHandlers(sortOrderSlider, cancelButton, fetchAndUpdateTable, navigateBack(currentRoot, setCurrentRoot, fetchAndUpdateTable), currentRoot);
+    addEventHandlers(sortOrderSlider, cancelButton, fetchAndUpdateTable, navigateBack(getCurrentRoot, setCurrentRoot, fetchAndUpdateTable));
 
     // Начальный запрос данных при загрузке страницы.
     fetchAndUpdateTable();
--- view/js/navigate.js
+++ view/js/navigate.js
@@ -1,36 +1,27 @@
 // Экспортируемая функция navigateToDirectory используется для перехода в указанную директорию
 // Параметры:
-// - currentRoot: текущий путь к директории
+// - getCurrentRoot: функция для получения текущего пути к директории
 // - setCurrentRoot: функция для обновления текущего пути
 // - fetchData: функция для получения данных с сервера
-
-export const navigateToDirectory = (currentRoot, setCurrentRoot, fetchData) => (dirName) => {
-    // Обновляем текущий путь на основе выбранной директории
+export const navigateToDirectory = (getCurrentRoot, setCurrentRoot, fetchAndUpdateTable) => (dirName) => {
+    const currentRoot = getCurrentRoot();
     const newRoot = currentRoot === '/' ? `/${dirName}` : `${currentRoot}/${dirName}`;
-    // Устанавливаем новый текущий путь
     setCurrentRoot(newRoot);
-    // Получаем данные для новой директории
-    fetchData(newRoot);
+    fetchAndUpdateTable();
 };
 
 // Экспортируемая функция navigateBack используется для возврата к предыдущей директории
 // Параметры:
-// - currentRoot: текущий путь к директории
+// - getCurrentRoot: функция для получения текущего пути к директории
 // - setCurrentRoot: функция для обновления текущего пути
 // - fetchData: функция для получения данных с сервера.
+export const navigateBack = (getCurrentRoot, setCurrentRoot, fetchAndUpdateTable) => () => {
+    const currentRoot = getCurrentRoot();
+    if (currentRoot === '/') return;
 
-export const navigateBack = (currentRoot, setCurrentRoot, fetchData) => () => {
-    // Если мы уже в корневом каталоге, ничего не делаем.
-    // if (currentRoot === '/') return;
-
-    // Разделяем путь на части и удаляем пустые элементы
     const pathParts = currentRoot.split('/').filter(part => part.length > 0);
-    // Удаляем последний элемент пути (текущую директорию)
     pathParts.pop();
-    // Обновляем текущий путь
     const newRoot = pathParts.length > 0 ? `/${pathParts.join('/')}` : '/';
-    // Устанавливаем новый текущий путь
     setCurrentRoot(newRoot);
-    // Получаем данные для новой директории
-    fetchData(newRoot);
+    fetchAndUpdateTable();
 };
